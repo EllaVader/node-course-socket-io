94730afb86f3f2f5ab43f80691aeb367b6dae503
EllaVader <jroupon@yahoo.com>
2017-11-24 16:24:49 -0500

    adding listener for server and client

--- server/server.js
+++ server/server.js
@@ -7,29 +7,30 @@ const express = require('express');
 To use socketIO, we have to use http directly rather than express.
 Express uses http for us, but we need to configure http to use socketIO
 */
+
 //path.join is a cleaner way to deal with moving around folders within your app
 const publicPath = path.join(__dirname, '../public');
 
 //process.env stores all of our environment variables
-//heroku will change the port based on on the PORT env variable
+//heroku uses this will change the port based on on the PORT env variable
 const port = process.env.PORT || 3000; //if PORT doesn't exist, default to port 3000
 //create new express app
 var app = express();
-//pass in the express app since it has all we need for http.createServer
+//pass in the express app to http create since since it has all we need for http.createServer
 var server = http.createServer(app);
 var io = socketIO(server); //websocket server to commuicate between client and server
-//configure middleware to set up the directory for all our public files
+
+/configure middleware to set up the directory for all our public files
 app.use(express.static(publicPath));
 
 //register an event listener
 //listen for a new connection
 io.on('connection', (socket) => {
   console.log('New user connected');
-  //listen for an closed connection on the socket connection
+  //listen for a closed connection on the socket connection (i.e browser closes)
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
-
 
 server.listen(port, () => console.log(`Server is up on port ${port}`));
